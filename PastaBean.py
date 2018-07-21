c818a414fc228c7c46d0a4570278485d17956f4e
Tu5k4rr <rory.vendee@gmail.com>
2018-07-21 18:06:33 +0100

    https item url update
    - Didnt add second https scrape item url - (https://scrape.pastebin.com/api_scrape_item.php?i=)
    - Removed gmail comments out to clean up code.
    - Fixed indentation error on last regex match.
    - Put each regex match in alphabetical order to make easier to manage for adding more.
    - Scripted tested on laptop with white listed IP.

--- PastaBean.py
+++ PastaBean.py
@@ -11,58 +11,52 @@ while True:
 	json_posts = json.loads(last_n_posts)
 
 	for post in json_posts:
-			raw_post_text = requests.get('http://pastebin.com/api_scrape_item.php?i='+ post['key'])
+			raw_post_text = requests.get('https://scrape.pastebin.com/api_scrape_item.php?i='+ post['key'])
 		
 			if re.match('(password|leak|dump)', raw_post_text.text, re.IGNORECASE) is not None :
-				w = open('{0}-{1}.txt'.format(post['key'], 'Common'), 'w')
-				w.write(raw_post_text.text.encode('utf-8').strip())
-				w.close()
-				###gmail_alert('Common',post['full_url'])
+				a = open('{0}-{1}.txt'.format(post['key'], 'Common'), 'w')
+				a.write(raw_post_text.text.encode('utf-8').strip())
+				a.close()
 			elif re.match('TV(oA|pB|pQ|qA|qQ|ro)', raw_post_text.text) is not None :
-				f = open('{0}-{1}.txt'.format(post['key'], 'PE64'), 'w')
-				f.write(raw_post_text.text.encode('utf-8').strip())
-				f.close()
-				###gmail_alert('Base64 Encoded Exe',post['full_url'])
+				b = open('{0}-{1}.txt'.format(post['key'], 'PE64'), 'w')
+				b.write(raw_post_text.text.encode('utf-8').strip())
+				b.close()
 			elif re.match('sdcard', raw_post_text.text, re.IGNORECASE) is not None :
-				y = open('{0}-{1}.txt'.format(post['key'], 'sdcard'), 'w')
-				y.write(raw_post_text.text.encode('utf-8').strip())
-				y.close()
-				###gmail_alert('sdcard',post['full_url'])			
+				c = open('{0}-{1}.txt'.format(post['key'], 'sdcard'), 'w')
+				c.write(raw_post_text.text.encode('utf-8').strip())
+				c.close()
 			elif re.match('.*.\.onion', raw_post_text.text, re.IGNORECASE) is not None :
-				z = open('{0}-{1}.txt'.format(post['key'], 'onion'), 'w')
-				z.write(raw_post_text.text.encode('utf-8').strip())
-				z.close()
-				###gmail_alert('Onion URL',post['full_url'])			
+				d = open('{0}-{1}.txt'.format(post['key'], 'onion'), 'w')
+				d.write(raw_post_text.text.encode('utf-8').strip())
+				d.close()
 			elif re.match('(STRING|ENTER|DELAY)',  raw_post_text.text, re.IGNORECASE) is not None :
-				x = open('{0}-{1}.txt'.format(post['key'], 'Ducky'), 'w')
-				x.write(raw_post_text.text.encode('utf-8').strip())
-				x.close()
-				###gmail_alert('Ducky',post['full_url'])
-			elif re.match('aHR0cDovLw', raw_post_text.text) is not None :
-				q = open('{0}-{1}.txt'.format(post['key'], 'httpB64'), 'w')
-				q.write(raw_post_text.text.encode('utf-8').strip())
-				q.close()
-				###gmail_alert('http64',post['full_url'])
-			elif re.match('QUACK.*.|(powershell|bash)|(LED.*.)|(ATTACKMODE.*.)', raw_post_text.text, re.IGNORECASE) is not None :
-				e = open('{0}-{1}.txt'.format(post['key'], 'BASH-BUNNY'), 'w')
+				e = open('{0}-{1}.txt'.format(post['key'], 'Ducky'), 'w')
 				e.write(raw_post_text.text.encode('utf-8').strip())
 				e.close()
-				###gmail_alert('Bash Bunny',post['full_url'])			
+			elif re.match('aHR0cDovLw', raw_post_text.text) is not None :
+				f = open('{0}-{1}.txt'.format(post['key'], 'httpB64'), 'w')
+				f.write(raw_post_text.text.encode('utf-8').strip())
+				f.close()
+			elif re.match('QUACK.*.|(powershell|bash)|(LED.*.)|(ATTACKMODE.*.)', raw_post_text.text, re.IGNORECASE) is not None :
+				g = open('{0}-{1}.txt'.format(post['key'], 'BASH-BUNNY'), 'w')
+				g.write(raw_post_text.text.encode('utf-8').strip())
+				g.close()
 			elif re.match('(IEX|iex|SUVY|aWV4)', raw_post_text.text) is not None :
 				h = open('{0}-{1}.txt'.format(post['key'], 'PS-IEX'), 'w')
 				h.write(raw_post_text.text.encode('utf-8').strip())
 				h.close()
-				###gmail_alert('iex',post['full_url'])
 			elif re.match('powershell', raw_post_text.text, re.IGNORECASE) is not None :
-                                s = open('{0}-{1}.txt'.format(post['key'], 'PS-IGN'), 'w')
-                                s.write(raw_post_text.text.encode('utf-8').strip())
-                                s.close()
-                                ###gmail_alert('ps-ignorecase',post['full_url'])			
+				i = open ('{0}-{1}.txt'.format(post['key'], 'PS-NOR'), 'w')
+				i.write(raw_post_text.text.encode('utf-8').strip())
+				i.close()
+			
+       			
 			 
 	time.sleep(60)
 
  
  
+
 
 
 
