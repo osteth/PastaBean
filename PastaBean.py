0dd30d9b0d5c9b86cb218e51ca0a7e5e89ce5df8
Tu5k4rr <21346295+Tu5k4rr@users.noreply.github.com>
2020-01-07 14:39:02 +0000

    Update PastaBean.py
    File open required 'b' binary mode with 'w' when handling raw bytes.
    
    ref: https://docs.python.org/3/library/functions.html#open

--- PastaBean.py
+++ PastaBean.py
@@ -16,47 +16,47 @@ while True:
                         raw_post_text = requests.get('https://scrape.pastebin.com/api_scrape_item.php?i='+ post['key'])
 
                         if re.match('(password|leak|dump)', raw_post_text.text, re.IGNORECASE) is not None :
-                                a = open('{0}-{1}.txt'.format(post['key'], 'Common'), 'w')
+                                a = open('{0}-{1}.txt'.format(post['key'], 'Common'), 'wb')
                                 a.write(raw_post_text.text.encode('utf-8').strip())
                                 a.close()
                         elif re.match('TV(oA|pB|pQ|qA|qQ|ro)', raw_post_text.text) is not None :
-                                b = open('{0}-{1}.txt'.format(post['key'], 'PE64'), 'w')
+                                b = open('{0}-{1}.txt'.format(post['key'], 'PE64'), 'wb')
                                 b.write(raw_post_text.text.encode('utf-8').strip())
                                 b.close()
                         elif re.match('sdcard', raw_post_text.text, re.IGNORECASE) is not None :
-                                c = open('{0}-{1}.txt'.format(post['key'], 'sdcard'), 'w')
+                                c = open('{0}-{1}.txt'.format(post['key'], 'sdcard'), 'wb')
                                 c.write(raw_post_text.text.encode('utf-8').strip())
                                 c.close()
                         elif re.match('.*.\.onion', raw_post_text.text, re.IGNORECASE) is not None :
-                                d = open('{0}-{1}.txt'.format(post['key'], 'onion'), 'w')
+                                d = open('{0}-{1}.txt'.format(post['key'], 'onion'), 'wb')
                                 d.write(raw_post_text.text.encode('utf-8').strip())
                                 d.close()
                         elif re.match('GUI | WINDOWS |STRING |DELAY |REM|ENTER|SHIFT | ALT ',  raw_post_text.text) is not None :
-                                e = open('{0}-{1}.txt'.format(post['key'], 'Ducky'), 'w')
+                                e = open('{0}-{1}.txt'.format(post['key'], 'Ducky'), 'wb')
                                 e.write(raw_post_text.text.encode('utf-8').strip())
                                 e.close()
                         elif re.match('aHR0cDovLw', raw_post_text.text) is not None :
-                                f = open('{0}-{1}.txt'.format(post['key'], 'httpB64'), 'w')
+                                f = open('{0}-{1}.txt'.format(post['key'], 'httpB64'), 'wb')
                                 f.write(raw_post_text.text.encode('utf-8').strip())
                                 f.close()
                         elif re.match('QUACK |LED |ATTACKMODE |DUCKY\_LANG', raw_post_text.text, re.IGNORECASE) is not None :
-                                g = open('{0}-{1}.txt'.format(post['key'], 'BASH-BUNNY'), 'w')
+                                g = open('{0}-{1}.txt'.format(post['key'], 'BASH-BUNNY'), 'wb')
                                 g.write(raw_post_text.text.encode('utf-8').strip())
                                 g.close()
                         elif re.match('(IEX|iex|SUVY|aWV4)', raw_post_text.text) is not None :
-                                h = open('{0}-{1}.txt'.format(post['key'], 'PS-IEX'), 'w')
+                                h = open('{0}-{1}.txt'.format(post['key'], 'PS-IEX'), 'wb')
                                 h.write(raw_post_text.text.encode('utf-8').strip())
                                 h.close()
                         elif re.match('powershell|invoke\-expression|downloadstring', raw_post_text.text, re.IGNORECASE) is not None :
-                                i = open ('{0}-{1}.txt'.format(post['key'], 'PS-NOR'), 'w')
+                                i = open ('{0}-{1}.txt'.format(post['key'], 'PS-NOR'), 'wb')
                                 i.write(raw_post_text.text.encode('utf-8').strip())
                                 i.close()
                         elif re.match('[13][a-km-zA-HJ-NP-Z1-9]{25,34}', raw_post_text.text) is not None :
-                                j = open ('{0}-{1}.txt'.format(post['key'], 'BC-WAL'), 'w')
+                                j = open ('{0}-{1}.txt'.format(post['key'], 'BC-WAL'), 'wb')
                                 j.write(raw_post_text.text.encode('utf-8').strip())
                                 j.close()
                         elif re.match ('((([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\:[0-9]{1,5})\/[a-zA-Z0-9(-@%^#/])', raw_post_text.text) is not None :
-                                k = open ('{0}-{1}.txt'.format(post['key'], 'IP-WEB'), 'w')
+                                k = open ('{0}-{1}.txt'.format(post['key'], 'IP-WEB'), 'wb')
                                 k.write(raw_post_text.text.encode('utf-8').strip())
                                 k.close()
 
